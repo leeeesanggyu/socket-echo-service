803836370ffea7b5c840d9e2bab331c76de01ea7
salgu1998 <salgu1998@gmail.com>
2023-09-21 15:27:01 +0900

    feat: project setting

---
+++ src/main/java/com/dozn/socketecho/SocketEchoApplication.java
@@ -0,0 +1,13 @@
+package com.dozn.socketecho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketEchoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocketEchoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dozn/socketecho/SocketEchoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dozn.socketecho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocketEchoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
